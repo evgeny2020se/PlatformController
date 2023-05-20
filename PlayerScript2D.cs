af3510c8e37efb45c4550fd52d0451b0093611d4
EVGENY_SE <74773570+evgeny2020se@users.noreply.github.com>
2023-05-20 11:24:42 +0300

    Add files via upload

---
+++ PlayerScript2D.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+
+public class PlayerScript2D : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float HorizontalMove = 0f;
+    private bool FacingRight = true;
+
+    [Range(0f, 10f)] public float speed = 5f;
+    [Range(0f, 15f)] public float jumpForce = 8f;
+
+    [Header("Player Animator Settings")]
+    public Animator animator; 
+
+    [Space]
+    [Header("Ground Checker Settings")]
+    public bool isGrounded = false;
+    [Range(-5f, 5f)] public float checkGroundOfsetY = -1.8f;
+    [Range(0, 5f)] public float checkGroundRadius = 0.3f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+
+        animator.SetFloat("HorizontalMove", Mathf.Abs(HorizontalMove));
+
+        if(isGrounded == false)
+        {
+            animator.SetBool("Jumping", true);
+        }
+        else
+        { 
+            animator.SetBool("Jumping", false);
+        }
+
+        if (HorizontalMove < 0 && FacingRight) 
+        {
+            Flip();
+        }
+        else if(HorizontalMove > 0 && !FacingRight)
+        {
+            Flip();
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector2 targetVelocity = new Vector2(HorizontalMove, rb.velocity.y);
+        rb.velocity = targetVelocity;
+
+        CheckGround();
+    }
+
+    private void Flip()
+    {
+        FacingRight = !FacingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll
+            (new Vector2(transform.position.x, transform.position.y + checkGroundOfsetY), checkGroundRadius);
+        if (colliders.Length > 1)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
